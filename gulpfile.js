25890dd34107c244d5d047f771da47718de2b139
Andre Moura <andremarquesmoura@gmail.com>
2019-01-25 13:20:07 -0800

    Add pseudocode to scripts

--- gulpfile.js
+++ gulpfile.js
@@ -4,41 +4,55 @@ const gulp = require("gulp"),
   terser = require("gulp-terser"),
   eslint = require("gulp-eslint"),
   prettyError = require("gulp-prettyerror"),
-  browserSync = require("browser-sync").create();
+  browserSync = require("browser-sync").create(),
+  sass = require("gulp-sass"),
+  autoprefixer = require("gulp-autoprefixer");
 
-// Task to minify CSS
-gulp.task("css", function() {
+// Task to compiling and minifying Sass
+gulp.task("sass", function() {
   return gulp
-    .src("./css/*.css")
+    .src("./sass/*.scss")
+    .pipe(prettyError())
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./build/css"))
     .pipe(uglifycss())
     .pipe(rename({ extname: ".min.css" }))
     .pipe(gulp.dest("./build/css"));
 });
 
 // Linting
-gulp.task('lint', function() {
-    return gulp.src("js/*.js")
+gulp.task("lint", function() {
+  return gulp
+    .src(["./js/*.js"])
     .pipe(prettyError())
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(eslint.failOnError());
+    .pipe(eslint.failAfterError());
 });
 
 // Task to minify JS
-gulp.task("js", function() {
-  return gulp
-    .src("./js/*.js")
-    .pipe(terser())
-    .pipe(rename({ extname: ".min.js" }))
-    .pipe(gulp.dest("./build/js"));
-});
+gulp.task(
+  "js",
+  gulp.series("lint", function() {
+    return gulp
+      .src("./js/*.js")
+      .pipe(terser())
+      .pipe(rename({ extname: ".min.js" }))
+      .pipe(gulp.dest("./build/js"));
+  })
+);
 
 // Task to watch for changes to CSS and JS files
 gulp.task("watch", function(done) {
-    gulp.watch("js/*.js", gulp.series("js"));
-    gulp.watch("css/*.css", gulp.series("css"));
-    done();
-  });
+  gulp.watch("sass/*.scss", gulp.series("sass"));
+  gulp.watch("js/*.js", gulp.series("js"));
+  done();
+});
 
 // Load browser-sync
 gulp.task("browser-sync", function(done) {
@@ -47,7 +61,9 @@ gulp.task("browser-sync", function(done) {
       baseDir: "./"
     }
   });
-  gulp.watch(["build/css/*.css", "build/js/*.js"]).on("change", browserSync.reload);
+  gulp
+    .watch(["build/css/*.css", "build/js/*.js"])
+    .on("change", browserSync.reload);
   done();
 });
 
--- js/script.js
+++ js/script.js
@@ -1,5 +1,55 @@
 $(function() {
   console.log("Yup! We are connected!");
 
-  console.log("browser-sync is working!");
+  // Problem: Retrieve content from the NYT Top Stories API and add it to our site.
+  // If we don't get a successful response, let the user know it.
+
+  // 1a. Listen for the select menu to change (watching its value)
+
+  $("news_sections").on("change", function() {
+    const section = $(this).val();
+    console.log(section);
+    if (section === "") return;
+
+    //show leader
+    //clear stories
+
+    // make our ajax request
+    $.ajax({
+      method: "GET",
+      dataType: "jason",
+      url:
+        "https://api.nytimes.com/svc/topstories/v2/" +
+        section +
+        ".json?api-key=N8PTNVMsydAsVz1zZpGLuJV22NbJOlQB"
+    })
+      .done(function(response) {
+        // append all the things
+        // 1. filter out articles without an image
+        // 2. create .each to run a function for each article in response.results
+        // 3. for each article, create constants for image URL, title and link
+        // 4. make an HTML string for the article, using the constants we just created
+        // 5. append strings to stories section
+      })
+      .fail(function() {
+        // do stuff here if it doesn't work out
+      })
+      .always(function() {
+        // hide the loader
+      });
+  });
+
+  // 1b. If the select value is "" do nothing and return from the function immediately.
+
+  // 1c. Show a loader to the user clear out old stories.
+
+  // 2. Send a request to the NYT API for data based on the value of the select menu
+
+  // 3. If successful, parse the data we get back and decide what parts we want to append to our DOM
+
+  // 4. Append that stuff to the DOM
+
+  // 5. If unsuccessful, append and show a helpful error message to the user in the UI
+
+  // 6. Hide the loader again.
 });
