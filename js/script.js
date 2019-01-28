3de967f86a62931e203f7fd3165870f4b1380304
Andre Moura <andremarquesmoura@gmail.com>
2019-01-27 18:48:28 -0800

    Add NYT API query results

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $(function() {
-  console.log("Yup! We are connected!");
+  // console.log("Yup! We are connected!");
 
   // Problem: Retrieve content from the NYT Top Stories API and add it to our site.
   // If we don't get a successful response, let the user know it.
 
   // 1a. Listen for the select menu to change (watching its value)
 
-  $("news_sections").on("change", function() {
+  $(".selector").on("change", function() {
     const section = $(this).val();
-    console.log(section);
+    // console.log(section);
     if (section === "") return;
 
     //show leader
@@ -17,26 +17,39 @@ $(function() {
     // make our ajax request
     $.ajax({
       method: "GET",
-      dataType: "jason",
+      dataType: "json",
       url:
         "https://api.nytimes.com/svc/topstories/v2/" +
         section +
         ".json?api-key=N8PTNVMsydAsVz1zZpGLuJV22NbJOlQB"
-    })
-      .done(function(response) {
-        // append all the things
-        // 1. filter out articles without an image
-        // 2. create .each to run a function for each article in response.results
-        // 3. for each article, create constants for image URL, title and link
-        // 4. make an HTML string for the article, using the constants we just created
-        // 5. append strings to stories section
-      })
-      .fail(function() {
-        // do stuff here if it doesn't work out
-      })
-      .always(function() {
-        // hide the loader
+    }).done(function(data) {
+      // console.log(data.results[0].title);
+      // append all the things
+      // 1. filter out articles without an image
+      // 2. create .each to run a function for each article in response.results
+      $.each(data.results, function(key, value) {
+        // console.log(value.title);
+        $(".articles").append(
+          '<a href="' +
+            value.url +
+            '" target="_blank">' +
+            "<li>" +
+            value.title +
+            "<img src=" +
+            value.multimedia[4].url +
+            "></li>"
+        );
       });
+      // 3. for each article, create constants for image URL, title and link
+      // 4. make an HTML string for the article, using the constants we just created
+      // 5. append strings to stories section
+      // })
+      // .fail(function() {
+      // do stuff here if it doesn't work out
+      // })
+      // .always(function() {
+      // hide the loader
+    });
   });
 
   // 1b. If the select value is "" do nothing and return from the function immediately.
