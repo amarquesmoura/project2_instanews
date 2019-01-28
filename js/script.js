d733c2f454a6d4254d317f1fc36928143af9ceb4
Andre Moura <andremarquesmoura@gmail.com>
2019-01-28 10:39:43 -0800

    Add typography

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ $(function() {
   $(".selector").on("change", function() {
     const section = $(this).val();
     // console.log(section);
+
+    // 1b. If the select value is "" do nothing and return from the function immediately.
     if (section === "") return;
 
-    //show leader
+    // 1c. Show a loader to the user clear out old stories.
     $(".loader").toggle();
-
-    //clear stories
     $(".articles").empty();
 
     // make our ajax request
@@ -28,46 +28,40 @@ $(function() {
     })
       .done(function(data) {
         // console.log(data.results[0].title);
+
         // append all the things
         // 1. filter out articles without an image
         // 2. create .each to run a function for each article in response.results
-        $.each(data.results, function(key, value) {
-          // console.log(value.title);
-          $(".articles").append(
-            '<a href="' +
-              value.url +
-              '" target="_blank">' +
-              "<li>" +
-              value.title +
-              "<img src=" +
-              value.multimedia[4].url +
-              "></li>"
-          );
-        });
         // 3. for each article, create constants for image URL, title and link
         // 4. make an HTML string for the article, using the constants we just created
         // 5. append strings to stories section
+
+        $.each(data.results, function(key, value) {
+          if (value.multimedia !== "[]") {
+            $(".articles").append(
+              '<a href="' +
+                value.url +
+                '" target="_blank">' +
+                "<li>" +
+                value.title +
+                "<img src=" +
+                value.multimedia[4].url +
+                "></li>"
+            );
+          } else {
+            $.append(
+              '<p class="error">Your search produced no results. Please try another subject.</p>'
+            );
+          }
+        });
       })
-      // .fail(function() {
-      // do stuff here if it doesn't work out
-      // })
-      .always(function() {
-        // hide the loader
-        $(".loader").toggle();
-      });
+      .fail(
+        function() {
+          $.alert("Something went wrong. Please try again.");
+        }.always(function() {
+          // hide the loader
+          $(".loader").toggle();
+        })
+      );
   });
-
-  // 1b. If the select value is "" do nothing and return from the function immediately.
-
-  // 1c. Show a loader to the user clear out old stories.
-
-  // 2. Send a request to the NYT API for data based on the value of the select menu
-
-  // 3. If successful, parse the data we get back and decide what parts we want to append to our DOM
-
-  // 4. Append that stuff to the DOM
-
-  // 5. If unsuccessful, append and show a helpful error message to the user in the UI
-
-  // 6. Hide the loader again.
 });
